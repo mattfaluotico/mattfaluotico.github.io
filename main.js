c7ffbe851592cc22220f0072c43ddaaa7c82a346
mattfaluotico <matt.faluotico@gmail.com>
2015-02-19 17:18:53 -0500

    about and work page

--- main.js
+++ main.js
@@ -1,20 +1,31 @@
 jQuery(document).ready(function($) {
 	
-	var $about = $(".about");
-	var $work = $(".work");
-	var $header = $(".intro");
-	var headerOffser = $(".hello").offset().top;
+	// var $about = $(".about");
+	// var $work = $(".work");
+	// var $header = $(".intro");
 
-	$(window).scroll(function(event) {
+	// $("#about-button").click(function(event) {
+	// 	event.preventDefault();
+ //    $('html, body').animate({
+ //        scrollTop: $("#scroll-catch").offset().top
+ //    }, 500);
+	// });
+
+	// 	$("#work-button").click(function(event) {
+	// 	event.preventDefault();
+ //    $('html, body').animate({
+ //        scrollTop: 1000
+ //    }, 500);
+	// });
+
+	// $(window).scroll(function(event) {
 		
-		var pos = $(window).scrollTop();	
-		var margin = 500 - pos*0.6;
+	// 	var pos = $(window).scrollTop();	
+	// 	var margin = 500 - pos*0.8;
 
-		if (margin > 70) {
-			$about.css('margin-top', margin);
-		} 
-
-
-	});
+	// 	if (margin > 30) {
+	// 		$about.css('margin-top', margin);
+	// 	} 
+	// });
 
 });
