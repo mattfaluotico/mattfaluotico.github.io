7a38b0e9ea5a615d3b3f51ad0a1a6ee09ac5dbba
mattfxyz <matt.faluotico@gmail.com>
2015-09-12 17:19:25 -0400

    removed comments from intro

--- js/intro.js
+++ js/intro.js
@@ -1,22 +1,22 @@
-// $(document).ready(function() {
+$(document).ready(function() {
 	
-// 	var $body = $("body");
+	var $body = $("body");
 
-// 	$(".il").hover(function into() {
-// 		var id = $(this).attr("id");
-// 		$(".il").addClass("hide");
-// 		$(this).removeClass("hide");
-// 		$body.addClass(id);
-// 	}, function out() {
-// 		$body.removeClass($(this).attr("id"));
-// 		var id = $(this).attr("id");
-// 		$(".il").removeClass("hide");
-// 	})
+	$(".il").hover(function into() {
+		var id = $(this).attr("id");
+		$(".il").addClass("hide");
+		$(this).removeClass("hide");
+		$body.addClass(id);
+	}, function out() {
+		$body.removeClass($(this).attr("id"));
+		var id = $(this).attr("id");
+		$(".il").removeClass("hide");
+	})
 
-// $(".social").hover(function() {
-// 		$("header, .hmu").addClass("hide");
-// 	}, function() {
-// 		$("header, .hmu").removeClass("hide");
-// 	});
+$(".social").hover(function() {
+		$("header, .hmu").addClass("hide");
+	}, function() {
+		$("header, .hmu").removeClass("hide");
+	});
 
-// });
+});
