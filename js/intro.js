c869aeb75f59c9d28252c83f1c7435187ae60c5a
mattfxyz <matt.faluotico@gmail.com>
2015-10-09 15:51:09 -0400

    lots of social links

--- js/intro.js
+++ js/intro.js
@@ -1,22 +1,22 @@
-$(document).ready(function() {
-	
-	var $body = $("body");
-
-	$(".il").hover(function into() {
-		var id = $(this).attr("id");
-		$(".il").addClass("hide");
-		$(this).removeClass("hide");
-		$body.addClass(id);
-	}, function out() {
-		$body.removeClass($(this).attr("id"));
-		var id = $(this).attr("id");
-		$(".il").removeClass("hide");
-	})
-
-$(".social").hover(function() {
-		$("header, .hmu").addClass("hide");
-	}, function() {
-		$("header, .hmu").removeClass("hide");
-	});
-
-});
+// $(document).ready(function() {
+//
+// 	var $body = $("body");
+//
+// 	$(".il").hover(function into() {
+// 		var id = $(this).attr("id");
+// 		$(".il").addClass("hide");
+// 		$(this).removeClass("hide");
+// 		$body.addClass(id);
+// 	}, function out() {
+// 		$body.removeClass($(this).attr("id"));
+// 		var id = $(this).attr("id");
+// 		$(".il").removeClass("hide");
+// 	})
+//
+// $(".social").hover(function() {
+// 		$("header, .hmu").addClass("hide");
+// 	}, function() {
+// 		$("header, .hmu").removeClass("hide");
+// 	});
+//
+// });
